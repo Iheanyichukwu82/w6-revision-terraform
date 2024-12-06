26ea7754009217939ff70372f6a1a3c3bfda2d1d
Iheanyichukwu <calistdonaogu_82@yahoo.com>
2024-12-06 11:10:52 -0500

    change

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "week6-io-bucket-terraform"
+    bucket = "wk6-io-bckit-terraform"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "tf-look"
     encrypt = true
 
   }
